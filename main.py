dc8ac752b8004d43b7ec7293676beff93c8ac463
Tymur <timurels645@gmail.com>
2025-09-08 14:51:29 +0200

    Some tests for gui

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
-print(12)
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout
+
+class MyWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Signals & Slots")
+
+        # Widgets
+        self.label = QLabel("Press the button")
+        self.button = QPushButton("Click Me!")
+
+        # Connect button click to a function
+        self.button.clicked.connect(self.on_button_click)
+
+        # Layout
+        layout = QVBoxLayout()
+        layout.addWidget(self.label)
+        layout.addWidget(self.button)
+        self.setLayout(layout)
+
+    def on_button_click(self):
+        if self.label.text() == "Press the button":
+            self.label.setText("It was clicked for first time!")
+        else:
+            self.label.setText("Button was clicked!")
+
+app = QApplication(sys.argv)
+screen = MyWindow()
+screen.show()
+sys.exit(app.exec_())
